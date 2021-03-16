91eb978e29fee172a29bdd8246eae505cf90350d
Anton Nikolaev <anton.nikolaev.kazan@gmail.com>
2021-03-16 21:52:26 +0300

    Create customer entities
    Create "Customers" folder with all base entities for purchases functionality.

---
+++ src/ApplicationCore/Entities/Customers/Invoice.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopWeb.ApplicationCore.Entities.Customers
+{
+    public class Invoice : BaseEntity, IAggregateRoot
+    {
+    }
+}
