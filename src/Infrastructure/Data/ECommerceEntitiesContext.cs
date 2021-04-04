e1ffa72cf260410796ab488dfe90d739ccc157f2
Anton Nikolaev <anton.nikolaev.kazan@gmail.com>
2021-04-04 21:26:48 +0300

    Create Employee entity

--- src/Infrastructure/Data/ECommerceEntitiesContext.cs
+++ src/Infrastructure/Data/ECommerceEntitiesContext.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.eShopWeb.ApplicationCore.Entities.Customers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Microsoft.eShopWeb.Infrastructure.Data
 {
---
+++ src/Infrastructure/Data/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.eShopWeb.ApplicationCore.Entities.EmployeeEntities;
+
+namespace Microsoft.eShopWeb.Infrastructure.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.\sqlexpress;Initial Catalog=ECommerceStoreDB;Integrated Security=True");
+        }
+    }
+}
